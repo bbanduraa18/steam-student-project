7303ff3f8f5736452cb1c337b51eec615603dbb8
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-17 21:57:42 +0200

    add and config auth with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCnuTELWPjRIu59FzC6Qf9U9LIntuZC4l4",
+    authDomain: "steam-project-auth.firebaseapp.com",
+    projectId: "steam-project-auth",
+    storageBucket: "steam-project-auth.appspot.com",
+    messagingSenderId: "952188772857",
+    appId: "1:952188772857:web:e25b7d494531789ededd28"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCnuTELWPjRIu59FzC6Qf9U9LIntuZC4l4",
+    authDomain: "steam-project-auth.firebaseapp.com",
+    projectId: "steam-project-auth",
+    storageBucket: "steam-project-auth.appspot.com",
+    messagingSenderId: "952188772857",
+    appId: "1:952188772857:web:e25b7d494531789ededd28"
+  }
 };
 
 /*
