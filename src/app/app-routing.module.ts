42dae1b8783af098eb439bc842614a83198b9485
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-18 13:19:46 +0200

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GamesComponent } from "./components/games/games.component";
+import { FriendsComponent } from "./components/friends/friends.component";
+import { LibraryComponent } from "./components/library/library.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { SignInComponent } from "./components/sign-in/sign-in.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'friends', component: FriendsComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
