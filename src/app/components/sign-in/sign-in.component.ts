250987d4b1cd94cb08e82534bb5e8eb628444f32
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-18 17:30:35 +0200

    add errorMessage method and Onsubmit method

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
+  @Input()
+  error?: string | null;
 
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password =  new FormControl('');
 
-  ngOnInit(): void {
+  constructor(private AuthService : AuthService, private route: Router) {
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required') || this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  onSubmit(): void {
+    this.AuthService.signIn(this.email.value, this.password.value)
+    this.route.navigate(['/games'])
+    console.log('welcome home!');
   }
 
 }
