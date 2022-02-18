56c2e22bff93dcbc1af16a52c8b55b98c056ad96
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-18 12:52:36 +0200

    create and import empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { GamesComponent } from './components/games/games.component';
+import { LibraryComponent } from './components/library/library.component';
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SignInComponent,
+    SignUpComponent,
+    ProfileComponent,
+    FriendsComponent,
+    GamesComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
