6acd435635197b4cdda8a87cd7c662119eab6967
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-20 16:05:26 +0200

    refactor auth service and add new method for logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from "@angular/fire/compat/auth";
+import {Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, authState} from "@angular/fire/auth";
+import { from } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   public isLoggedIn: boolean;
+  currentUser$ = authState(this.auth)
 
-  constructor(private AngularFireAuth: AngularFireAuth) {
+  constructor(private auth: Auth) {
     this.isLoggedIn = false;
   }
 
   signIn(email: string, password: string) {
-    this.AngularFireAuth.signInWithEmailAndPassword(email, password)
-      .then(r => console.log(r));
-
     this.isLoggedIn = true;
+    return from(signInWithEmailAndPassword(this.auth, email, password))
   }
 
-  signUp(email: string, password: string) {
-    this.AngularFireAuth.createUserWithEmailAndPassword(email, password)
-      .then(r => console.log(r))
+  logOut() {
+    return from(this.auth.signOut());
   }
 
 }
