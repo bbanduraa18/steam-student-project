9ea65f815d4fc534db6210287e325c1f42abc5ca
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-20 17:27:55 +0200

    complete sign-up component

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { HotToastService } from "@ngneat/hot-toast";
+import { Router } from "@angular/router";
+
+export function passwordMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if(password && confirmPassword && password !== confirmPassword) {
+      return {
+        passwordsDontMatch: true
+      }
+    };
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +26,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email ,Validators.required]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  }, { validators: passwordMatchValidator() })
+
+  constructor(private auth: AuthService,
+              private toast: HotToastService,
+              private router: Router ) { }
 
   ngOnInit(): void {
+  }
+
+  get username() {
+    return this.signUpForm.get('username');
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
+  }
+
+  get age() {
+    return this.signUpForm.get('age');
+  }
+
+  getErrorMessage() {
+    if (this.email?.hasError('required')
+      || this.password?.hasError('required')
+      || this.username?.hasError('required')
+      || this.confirmPassword?.hasError('required')
+      || this.age?.hasError('required')) {
+
+        return 'You must enter a value';
+
+    }
+
+    return this.email?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  onSubmit() {
+    if(this.signUpForm.valid) {
+      const { username, email, password } = this.signUpForm.value;
+      this.auth.sighUp(username, email, password).pipe(
+        this.toast.observe({
+          success: 'Congrats! You are all signed up!',
+          loading: 'Signing in',
+          error: ({ message }) => `${message}`
+        })
+      ).subscribe(() => {
+        this.router.navigate(['/profile']);
+      })
+    }
   }
 
 }
