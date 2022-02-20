18b715fc534a3c8d72ac8b34a6b8c4c9cfb4fbcd
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-20 16:03:56 +0200

    refactor toolbar component and his methods to protect routes

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
 
-  constructor() { }
+  constructor(public auth: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  logOut() {
+    this.auth.logOut().subscribe(() => {
+      this.router.navigate(['']);
+    });
   }
 
 }
