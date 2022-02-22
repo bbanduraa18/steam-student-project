0d30f024f1dc7530ff0ef465b47e13bef8c9f9e0
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-22 15:20:01 +0200

    test template to add and import/export friends to firebase

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-friends',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendsComponent implements OnInit {
 
-  constructor() { }
+  public friends?: any[];
+  public searchUser = new FormControl('');
+
+  constructor(private auth: AuthService,
+              private afStore: AngularFirestore) {
+  }
 
   ngOnInit(): void {
+    this.getFriends()
+  }
+
+  getFriends() {
+    this.auth.getCurrentUser().subscribe(user => {
+
+      this.afStore.collection('/users/' + user?.email + '/friends/').get().subscribe(snapshot => {
+        this.friends = snapshot.docs.map(doc => doc.data());
+        console.log(this.friends)
+      })
+    });
+  }
+
+  addFriend() {
+    this.auth.getCurrentUser().subscribe(user => {
+      console.log(this.searchUser.value)
+
+      this.afStore.doc('/users/' + user?.email + '/friends/' + this.searchUser.value).set({
+        email: this.searchUser.value
+      })
+      this.getFriends()
+    })
   }
 
 }
+
