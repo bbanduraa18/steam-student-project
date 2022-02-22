99e672b5ce05cd67fe8969af41eaa661fcd9c2db
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-22 19:14:43 +0200

    create crud operators for db

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../services/auth.service";
-import {AngularFirestore} from "@angular/fire/compat/firestore";
-import {FormControl} from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { FormControl } from "@angular/forms";
+import { arrayRemove, arrayUnion } from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-friends',
@@ -10,7 +11,10 @@ import {FormControl} from "@angular/forms";
 })
 export class FriendsComponent implements OnInit {
 
-  public friends?: any[];
+  public allUsers?: any[];
+  public friends?: any;
+  public indexOfUser: number = 0;
+  public user: any;
   public searchUser = new FormControl('');
 
   constructor(private auth: AuthService,
@@ -18,28 +22,45 @@ export class FriendsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getFriends()
-  }
-
-  getFriends() {
     this.auth.getCurrentUser().subscribe(user => {
-
-      this.afStore.collection('/users/' + user?.email + '/friends/').get().subscribe(snapshot => {
-        this.friends = snapshot.docs.map(doc => doc.data());
-        console.log(this.friends)
-      })
+      this.user = user?.email;
+      this.getFriends();
+    });
+    this.afStore.collection('/friends/').get().subscribe(snapshot => {
+      this.allUsers = snapshot.docs.map(doc => doc.data());
+      console.log(this.allUsers)
     });
   }
 
-  addFriend() {
-    this.auth.getCurrentUser().subscribe(user => {
-      console.log(this.searchUser.value)
-
-      this.afStore.doc('/users/' + user?.email + '/friends/' + this.searchUser.value).set({
-        email: this.searchUser.value
-      })
-      this.getFriends()
+  getFriends() {
+    this.afStore.collection('/users/').doc(this.user).get().subscribe(snapshot => {
+      this.friends = snapshot.data();
+      this.friends = this.friends.friends;
     })
+  }
+
+  searchUsers() {
+    const search = this.searchUser.value;
+    this.indexOfUser = this.allUsers!.findIndex((element) => element.email === search);
+  }
+
+  addFriend(userEmail: string | null) {
+    if(this.friends.includes(userEmail)) {
+      return null;
+    }
+
+    this.afStore.collection('/users/').doc(this.user).update({
+      friends: arrayUnion(userEmail)
+    }).then(() => console.log(`'${userEmail}' was added to your friends.`))
+    this.getFriends();
+    return;
+  }
+
+  deleteFriend(friend: string) {
+    this.afStore.collection('/users/').doc(this.user).update({
+      friends: arrayRemove(friend)
+    }).then(() => console.log(`'${friend}' was removed from your friends.`));
+    this.getFriends();
   }
 
 }
