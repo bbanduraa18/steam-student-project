e396b64e61adff3092495e979ca02605cd2df4ec
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-24 01:42:14 +0200

    complete library component

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-library',
@@ -7,14 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
+  public user: any;
+  public games?: any;
+  public allGames?: any[];
+  public localGames: any[] = [];
+  private indexesOfGames: number[] = [];
   public spinner: boolean = false;
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private afStore: AngularFirestore) { }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.spinner = true
-    }, 1000)
+    }, 1000);
+
+    this.auth.getCurrentUser().subscribe(user => {
+      this.user = user?.email;
+      this.getGames();
+    });
+    this.afStore.collection('/games/').get().subscribe(snapshot => {
+      this.allGames = snapshot.docs.map(doc => doc.data());
+    });
+  }
+
+  getGames() {
+    this.afStore.collection('/users/').doc(this.user).get().subscribe(snapshot => {
+      this.games = snapshot.data();
+      this.games = this.games.games;
+
+      this.indexesOfGames = this.getAllIndexes(this.games, this.allGames);
+      for(let i = 0; i < this.indexesOfGames!.length; i++) {
+        this.localGames.push(this.allGames![this.indexesOfGames![i]]);
+      }
+    })
+  }
+
+  getAllIndexes(val: [], arr?: any[]) {
+    let indexes = [];
+    for(let i = 0; i < arr!.length; i++){
+      for(let j = 0; j < val.length; j++) {
+        if(arr![i].title.includes(val[j])) {
+          indexes.push(i);
+        }
+      }
+    }
+
+    return (indexes);
   }
 
 }
