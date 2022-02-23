8e216eb6d2fe18c76a706ac4b702b661691ef2a1
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-23 03:10:18 +0200

    add loading spinner, refactor errors

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
 import { HotToastService } from "@ngneat/hot-toast";
@@ -36,7 +36,8 @@ export function ageValidator(reg: RegExp): ValidatorFn {
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  public spinner: boolean = false;
 
   signUpForm = new FormGroup({
     username: new FormControl('', Validators.required),
@@ -50,6 +51,12 @@ export class SignUpComponent {
               private toast: HotToastService,
               private router: Router,
               private firestore: AngularFirestore) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.spinner = true
+    }, 1000)
+  }
 
   get username() {
     return this.signUpForm.get('username');
