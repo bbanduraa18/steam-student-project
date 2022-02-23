276ef80e3ff0e2531dc392e0e7518b4542185914
Bandura Andrii <bbanduraa18@gmail.com>
2022-02-23 03:09:16 +0200

    add loading spinner, refactor errors

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../services/auth.service";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+
+export function ageValidator(reg: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if(reg.test(control.value)) {
+      return {
+        ageIsNotDigits: true
+      }
+    }
+
+    return null
+  }
+}
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +22,14 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 })
 export class ProfileComponent implements OnInit {
 
+  private currentUsername: string = '';
+  private currentAge: number = 0;
+  public spinner: boolean = false;
+
   userInfoForm = new FormGroup({
     username: new FormControl('', Validators.required),
     email: new FormControl({value: '', disabled: true}),
-    age: new FormControl('', Validators.required)
+    age: new FormControl('', [Validators.required, Validators.min(16), Validators.max(99), ageValidator(/\D/g)])
   })
 
   get username() {
@@ -28,34 +44,59 @@ export class ProfileComponent implements OnInit {
     return this.userInfoForm.get('age')?.value;
   }
 
+  get ageForError() {
+    return this.userInfoForm.get('age');
+  }
+
+  get usernameForError() {
+    return this.userInfoForm.get('username');
+  }
+
   constructor(private auth: AuthService,
               private afStore: AngularFirestore) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.spinner = true
+    }, 1000);
+
     this.auth.getCurrentUser().subscribe(user => {
 
       this.afStore.doc('/users/' + user?.email).get().subscribe(snapshot => {
         const userInfo: any = snapshot.data();
 
+        this.currentUsername = userInfo.username;
+        this.currentAge = userInfo.age;
+
         this.userInfoForm.controls['username'].setValue(userInfo.username);
         this.userInfoForm.controls['email'].setValue(userInfo.email);
         this.userInfoForm.controls['age'].setValue(userInfo.age);
       })
-    })
-  }
-
-  getErrorMessage() {
-    if (this.username?.hasError('required') || this.age?.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return;
+    });
   }
 
   onSubmit() {
-    this.afStore.doc('/users/' + this.email).update({
-      username: this.username,
-      age: this.age
-    })
+    if(this.currentUsername !== this.username && this.currentAge !== this.age) {
+      this.afStore.doc('/users/' + this.email).update({
+        username: this.username,
+        age: this.age
+      }).then(() => {
+        window.location.reload();
+      });
+    } else if(this.currentUsername !== this.username) {
+      this.afStore.doc('/users/' + this.email).update({
+        username: this.username
+      }).then(() => {
+        window.location.reload();
+      });
+    } else if (this.currentAge !== this.age) {
+      this.afStore.doc('/users/' + this.email).update({
+        age: this.age
+      }).then(() => {
+        window.location.reload();
+      });
+    }
+
+
   }
 }
